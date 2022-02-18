3e1cc05c7a52569cfd14d0515f55ad1c0c8df551
Hildegarht <hildaamadu@gmail.com>
2022-02-18 00:56:39 +0000

    extended home.html

--- App/urls.py
+++ App/urls.py
@@ -5,5 +5,4 @@ from . import views
 urlpatterns = [
     path('',views.index, name='index'),
     path('post/<str:pk>',views.post,name='post'),
-    path('home/<str:hm>',views.post,name='home')
 ]
--- App/views.py
+++ App/views.py
@@ -1,10 +1,6 @@
 from django.shortcuts import render
 from .models import Post
 
-def home(request,hm):
-    posts = Post.objects.get(id='hm')
-    return render(request,'home.html',{'posts':posts})
-
 def index(request):
     posts = Post.objects.all()
     return render(request,'index.html',{'posts':posts})
